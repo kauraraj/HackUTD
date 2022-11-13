01b76a2ea96d37ec722e200d2b678042130b8843
alexis <alexism.ng26@gmail.com>
2022-11-12 23:49:28 -0600

    purchases

--- my-app/src/components/bankroll/Bankroll.js
+++ my-app/src/components/bankroll/Bankroll.js
@@ -8,49 +8,6 @@ import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
 
-// function createData(
-//   item_payer_id,
-//   item_purchase_date,
-//   item_description,
-//   item_amount
-// ) {
-//   return { item_payer_id, item_purchase_date, item_description, item_amount };
-// }
-
-// const rows = [
-//   createData(
-//     item.payer_id[1],
-//     item.purchase_date[1],
-//     item.description[1],
-//     item.amount[1]
-//   ),
-//   createData(
-//     item.payer_id[2],
-//     item.purchase_date[2],
-//     item.description[2],
-//     item.amount[2]
-//   ),
-//   createData(
-//     item.payer_id[3],
-//     item.purchase_date[3],
-//     item.description[3],
-//     item.amount[3]
-//   ),
-//   createData(
-//     item.payer_id[4],
-//     item.purchase_date[4],
-//     item.description[4],
-//     item.amount[4]
-//   ),
-// ];
-
-// function createRows(){
-// const rows = [];
-// for (let i = 0; i < items.length; i++) {
-//     rows.push(createData(item.payer_id, item.purchase_date, item.description, item.amount));
-// }
-// return <tbody>{rows}</tbody>;
-
 export default function PullBankStatements() {
   /* Declaring a variable called error and setting it to null. */
   const [error, setError] = useState(null);
@@ -69,6 +26,15 @@ export default function PullBankStatements() {
         (result) => {
           setIsLoaded(true);
           setItems(result);
+          result.map((re) => {
+            return fetch(
+              "http://api.nessieisreal.com/merchants/" +
+                re.merchant_id +
+                "?key=a49d1d95d9ef2eeec64c6ef4639add8c"
+            )
+              .then((res) => res.json())
+              .then((merchants) => {}); // how to connect and map it to the others?
+          });
         },
         // Note: it's important to handle errors here
         // instead of a catch() block so that we don't swallow
@@ -86,15 +52,6 @@ export default function PullBankStatements() {
     return <div>Loading...</div>;
   } else {
     return (
-      // <Box>
-      //   <Typography variant="h1">Purchases</Typography>
-      //   {items.map((item) => (
-      //     <li key={item.payer_id}>
-      //       {item.purchase_date}: {item.description}, ${item.amount}
-      //     </li>
-      //   ))}
-      // </Box>
-
       <TableContainer component={Paper}>
         <Table sx={{ minWidth: 650 }} aria-label="simple table">
           <TableHead>
@@ -105,17 +62,18 @@ export default function PullBankStatements() {
             <TableCell align="right">Date</TableCell>
           </TableHead>
           <TableBody>
+            {/* Need to enable showing based on time-period */}
             {items.map((item) => (
               <TableRow
                 key={item.pay_id}
                 sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
               >
                 <TableCell component="th" scope="row">
-                  
+                  {item.name}
                 </TableCell>
                 <TableCell align="right">${item.amount}</TableCell>
+                <TableCell align="right">{item.description}</TableCell>
                 <TableCell align="right">Amount Left</TableCell>
-                <TableCell align="right">yu</TableCell>
                 <TableCell align="right">{item.purchase_date}</TableCell>
               </TableRow>
             ))}
--- my-app/src/containers/duringTimePeriod/duringTimePeriod.js
+++ my-app/src/containers/duringTimePeriod/duringTimePeriod.js
@@ -1,7 +1,10 @@
+import { Typography } from "@mui/material";
 import PullBankStatements from "../../components/bankroll/Bankroll";
 export default function DuringTimePeriod() {
   return (
     <div>
+      <Typography variant="h1">Purchases</Typography>
+
       <PullBankStatements></PullBankStatements>
     </div>
   );
